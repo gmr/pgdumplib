f99535f838ca6380c9fa007a583ed3d2382facb6
Gavin M. Roy <gavinr@aweber.com>
2018-11-06 17:06:58 -0500

    Include long description

--- pgdumplib/__init__.py
+++ pgdumplib/__init__.py
@@ -3,4 +3,4 @@ pgdumplib
 =========
 
 """
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
+from os import path
 import setuptools
 
 from pgdumplib import __version__
 
 
+def read_requirements(name):
+    requirements = []
+    try:
+        with open(path.join('requires', name)) as req_file:
+            for line in req_file:
+                if '#' in line:
+                    line = line[:line.index('#')]
+                line = line.strip()
+                if line.startswith('-r'):
+                    requirements.extend(read_requirements(line[2:].strip()))
+                elif line and not line.startswith('-'):
+                    requirements.append(line)
+    except IOError:
+        pass
+    return requirements
+
+
 setuptools.setup(
     name='pgdumplib',
     version=__version__,
-    description='Library for PostgreSQL backups created with pg_dump',
+    description='Library for reading PostgreSQL backups created with pg_dump',
+    long_description=open('README.rst').read(),
     author='Gavin M. Roy',
     author_email='gavinmroy@gmail.com',
     url='https://github.com/gmr/pgdumplib',
@@ -14,6 +33,7 @@ setuptools.setup(
     packages=['pgdumplib'],
     package_data={'': ['LICENSE', 'README.rst']},
     include_package_data=True,
+    tests_require=read_requirements('testing.txt'),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
