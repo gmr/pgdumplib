55e093207b2c73f20df9da8c27fcadd579eb1a11
Gavin M. Roy <gavinr@aweber.com>
2019-06-10 20:21:16 -0400

    Rework write test to read/write

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -1,41 +1,20 @@
+import logging
 import pathlib
-import tempfile
 import unittest
 
-from pgdumplib import constants, dump
+import pgdumplib
+
+LOGGER = logging.getLogger(__name__)
 
 
 class TestCase(unittest.TestCase):
 
-    def test_dump_construction(self):
-        path = pathlib.Path(tempfile.gettempdir()) / 'test.dump'
-        dmp = dump.Dump(path)
+    PATH = 'dump.not-compressed'
 
-        dmp.add_entry(
-            tag='ENCODING', desc='ENCODING',
-            section=constants.SECTION_PRE_DATA,
-            defn="SET client_encoding = 'UTF8';")
-        dmp.add_entry(
-            tag='STDSTRINGS', desc='STDSTRINGS',
-            section=constants.SECTION_PRE_DATA,
-            defn="SET standard_conforming_strings = 'on';")
-        dmp.add_entry(
-            tag='SEARCHPATH', desc='SEARCHPATH',
-            section=constants.SECTION_PRE_DATA,
-            defn="SELECT pg_catalog.set_config('search_path', '', false);")
+    def setUp(self):
+        self.local_path = pathlib.Path('build') / 'data' / self.PATH
+        self.dump = pgdumplib.load(self.local_path)
 
-        database = dmp.add_entry(
-            tag='postgres', desc='DATABASE', owner='postgres',
-            section=constants.SECTION_PRE_DATA,
-            defn='CREATE DATABASE postgres WITH TEMPLATE = template0 '
-                 "ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' "
-                 "LC_CTYPE = 'en_US.utf8';",
-            drop_stmt='DROP DATABASE postgres;')
-        dmp.add_entry(
-            tag='DATABASE postgres', desc='COMMENT', owner='postgres',
-            section=constants.SECTION_PRE_DATA,
-            defn="COMMENT ON DATABASE postgres IS 'default administrative "
-                 "connection database';",
-            dependencies=[database.dump_id])
-
-        dmp.save()
+    def test_redump(self):
+        self.dump.path = self.dump.path + '.test'
+        self.dump.save()
